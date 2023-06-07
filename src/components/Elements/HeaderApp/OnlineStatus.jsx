661ea8eb1747eb37d6d9d3c3e2c20e12b3614f42
ndrvndr <andreavindra37@gmail.com>
2023-06-07 14:12:52 +0700

    fix bug in online status

--- src/components/Elements/HeaderApp/OnlineStatus.jsx
+++ src/components/Elements/HeaderApp/OnlineStatus.jsx
@@ -1,14 +1,34 @@
 import PropTypes from "prop-types";
+import * as React from "react";
 
-import { HiStatusOnline } from "react-icons/hi";
+import { HiStatusOnline, HiStatusOffline } from "react-icons/hi";
 
 const OnlineStatus = ({ isInputFocused }) => {
+  const [isOnline, setIsOnline] = React.useState(navigator.onLine);
+  React.useEffect(() => {
+    const handleOnline = () => setIsOnline(true);
+    const handleOffline = () => setIsOnline(false);
+
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
+  }, []);
   return (
     <div
       className={`${isInputFocused ? "hidden" : "flex"} mt-2 flex items-center`}
     >
-      <HiStatusOnline color='#4ECE3D' />
-      <span className='ml-1 text-sm'>Terhubung</span>
+      {isOnline ? (
+        <HiStatusOnline color='#4ECE3D' />
+      ) : (
+        <HiStatusOffline color='#fff' />
+      )}
+      <span className='ml-1 text-sm'>
+        {isOnline ? "Terhubung" : "Tidak Terhubung"}
+      </span>
     </div>
   );
 };
