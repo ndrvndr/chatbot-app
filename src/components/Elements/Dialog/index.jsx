0229e1293aa34193045be54ca797c67124fd2710
ndrvndr <andreavindra37@gmail.com>
2023-06-11 22:53:05 +0700

    change z-index dialog

--- src/components/Elements/Dialog/index.jsx
+++ src/components/Elements/Dialog/index.jsx
@@ -18,34 +18,34 @@ export default function MyModal(props) {
 
   return (
     <Transition appear show={isOpen} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={closeModal}>
+      <Dialog as='div' className='relative z-20' onClose={closeModal}>
         <Transition.Child
           as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
+          enter='ease-out duration-300'
+          enterFrom='opacity-0'
+          enterTo='opacity-100'
+          leave='ease-in duration-200'
+          leaveFrom='opacity-100'
+          leaveTo='opacity-0'
         >
-          <div className="fixed inset-0 bg-black bg-opacity-25" />
+          <div className='fixed inset-0 bg-black bg-opacity-25' />
         </Transition.Child>
 
-        <div className="fixed inset-0 overflow-y-auto">
-          <div className="flex min-h-full items-center justify-center p-4 text-center">
+        <div className='fixed inset-0 overflow-y-auto'>
+          <div className='flex min-h-full items-center justify-center p-4 text-center'>
             <Transition.Child
               as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
+              enter='ease-out duration-300'
+              enterFrom='opacity-0 scale-95'
+              enterTo='opacity-100 scale-100'
+              leave='ease-in duration-200'
+              leaveFrom='opacity-100 scale-100'
+              leaveTo='opacity-0 scale-95'
             >
-              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+              <Dialog.Panel className='w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all'>
                 <Dialog.Title
-                  as="h3"
-                  className="flex items-center text-lg font-medium leading-6 text-gray-900"
+                  as='h3'
+                  className='flex items-center text-lg font-medium leading-6 text-gray-900'
                 >
                   <div className={`mr-3 rounded-full p-2 ${bgColor}`}>
                     {icon &&
@@ -56,13 +56,13 @@ export default function MyModal(props) {
                   </div>
                   {dialogTitle}
                 </Dialog.Title>
-                <div className="mt-2">
-                  <p className="text-sm text-gray-500">{dialogDesc}</p>
+                <div className='mt-2'>
+                  <p className='text-sm text-gray-500'>{dialogDesc}</p>
                 </div>
 
-                <div className="mt-5 flex justify-end">
+                <div className='mt-5 flex justify-end'>
                   <button
-                    type="button"
+                    type='button'
                     className={`inline-flex justify-center rounded-md border border-transparent font-medium text-white ${buttonColor} px-4 py-2 text-sm font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2`}
                     onClick={closeModal}
                   >
