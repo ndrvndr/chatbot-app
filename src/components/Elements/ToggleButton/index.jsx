e86e59bfd17a9e5c135e27c89f8c30637bc0fdd2
ndrvndr <andreavindra37@gmail.com>
2023-06-19 16:15:07 +0700

    adjust height chatbot app

--- src/components/Elements/ToggleButton/index.jsx
+++ src/components/Elements/ToggleButton/index.jsx
@@ -7,11 +7,11 @@ const ToggleButton = ({ isContentActive, toggleContent }) => {
     <div
       className={clsx(
         "absolute bottom-3 right-3",
-        "cursor-pointer rounded-[100%] border-[none] p-3.5",
+        "w-fit cursor-pointer rounded-[100%] border-[none] p-3.5",
         "bg-[#0e74bd] hover:bg-[#064a7a]",
         "transition-all duration-[1s]",
         isContentActive ? "hidden" : "block",
-        "min-[449px]:bottom-0 min-[449px]:right-0 min-[449px]:block"
+        "min-[449px]:static min-[449px]:block min-[449px]:self-end"
       )}
       onClick={() => toggleContent()}
     >
--- src/components/Fragments/ChatbotApp.jsx
+++ src/components/Fragments/ChatbotApp.jsx
@@ -43,7 +43,7 @@ const ChatbotApp = () => {
           "h-full w-full max-w-md",
           "bg-white",
           "ease transition-all duration-[0.5s]",
-          "min-[449px]:bottom-[70px] min-[449px]:right-6 min-[449px]:h-[630px] min-[449px]:max-w-[400px] min-[449px]:rounded-xl",
+          "min-[449px]:static min-[449px]:mb-3 min-[449px]:h-0 min-[449px]:max-w-[400px] min-[449px]:grow min-[449px]:rounded-xl",
           isContentActive ? "opacity-1" : "opacity-0"
         )}
       >
